d394cda6cdde5cb713e4499fec9b00588fe08cc5
dinhson2905 <dinhson2905@gmail.com>
2020-05-25 19:55:22 +0700

    a

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Sidebar from '../components/Sidebar';
 
 class DashBoard extends Component {
     render() {
         return (
             <div>
-                DashBoard
+                <Sidebar />
             </div>
         );
     }
